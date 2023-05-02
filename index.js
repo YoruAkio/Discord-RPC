46194ed830477dfc2db7dd7ff2491e4ae5da0bba
YoruAkio <shahrimalik1124@gmail.com>
2023-05-02 18:05:52 +0700

    🌟🎰 Adding properties for setActivity

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const RichPresence = require("discord-rpc")
-const browser = typeof window != 'undefined'
-const rpc = new RichPresence.Client({ transport: "ipc"})
-const config = require("./config.json")
+const RichPresence = require("discord-rpc");
+const browser = typeof window != "undefined";
+const rpc = new RichPresence.Client({
+    transport: browser ? "websocket" : "ipc",
+});
+const config = require("./config.js");
+const fs = require("fs");
+
+module.exports = rpc;
 
 rpc.on("ready", () => {
-	console.log("Running discord rpc")
+    rpc.setActivity({
+        details: config.Details,
+        state: config.State,
+        startTimestamp: new Date(),
+        largeImageKey: config.LargeImageKey,
+        largeImageText: config.LargeImageText,
+        smallImageKey: config.SmallImageKey,
+        smallImageText: config.SmallImageText,
+        buttons: [
+            {
+                label: config.Button1Label,
+                url: config.Button1URL,
+            },
+            {
+                label: config.Button2Label,
+                url: config.Button2URL,
+            },
+        ],
+        instance: true,
+    });
 
-	rpc.setActivity({
-		details: config.Details,
-		startTimestamp: new Date()
-	})
+    console.log(`Rich Presence is logged on: ${rpc.user.username}`);
+});
 
-	console.log(`Rich Presence is logged on: ${rpc.user.username}`)
-})
-
-rpc.login({ clientId: "967967669871579167" })
+rpc.login({ clientId: config.ClientID });
