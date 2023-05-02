b2775f8e834ad5d9dbe0d7bf99c118496580a4d4
YoruAkio <shahrimalik1124@gmail.com>
2023-05-02 18:02:37 +0700

    🪄🌈 Adding config file

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ClientID: "",
+    Details: "",
+    LargeImageKey: "",
+    LargeImageText: "",
+    SmallImageKey: "",
+    SmallImageText: "",
+    State: "",
+    Button1Label: "",
+    Button1URL: "",
+    Button2Label: "",
+    Button2URL: "",
+};
