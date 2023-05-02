b2c26f94daf256af9317ec5e22ef82b12f89d0b9
YoruAkio <shahrimalik1124@gmail.com>
2023-05-02 18:16:51 +0700

    🪄🌈 Add edit file trigger

--- config.js
+++ config.js
@@ -1,13 +1,24 @@
+const fs = require("fs");
+
 module.exports = {
-    ClientID: "",
-    Details: "",
-    LargeImageKey: "",
-    LargeImageText: "",
-    SmallImageKey: "",
-    SmallImageText: "",
-    State: "",
-    Button1Label: "",
-    Button1URL: "",
-    Button2Label: "",
-    Button2URL: "",
+    ClientID: "967967669871579167",
+    Details: "Waiting for you",
+    LargeImageKey: "https://pfps.gg/assets/pfps/6183-moon.png",
+    LargeImageText: "YoruAkio",
+    SmallImageKey:
+        "https://c.tenor.com/TgKK6YKNkm0AAAAi/verified-verificado.gif",
+    SmallImageText: "airi.dev",
+    State: "Eating chips (666 of 999)",
+    Button1Label: "Github",
+    Button1URL: "https://github.com/YoruAkio",
+    Button2Label: "Website",
+    Button2URL: "https://airi.dev",
 };
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(chalk.redBright(`Update ${__filename}`));
+    delete require.cache[file];
+    require(file);
+});
